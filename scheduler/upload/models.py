711a83cc3401ae5503e187fb2840d86b2f328d00
AudreyFelicio <audreyanwar@gmail.com>
2020-06-24 21:17:26 +0800

    modified note model and view

--- scheduler/upload/models.py
+++ scheduler/upload/models.py
@@ -26,10 +26,14 @@ class ImageHolder(models.Model):
         return self.name
 
 class Note(models.Model):
+    identifier = models.IntegerField(null=True)
     title = models.CharField(max_length=200)
     text = models.TextField()
     group = models.ForeignKey(
         EventGroup, on_delete=models.CASCADE, null=True, blank=True, related_name="notes")
+
+    class Meta:
+        unique_together = ['identifier', 'group']
 
     def __str__(self):
         return self.title
