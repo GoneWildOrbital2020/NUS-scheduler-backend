ba628c137441ecf38df390d2ceadf074293a5ae4
ZoroarkDarkrai <fidellawi@gmail.com>
2020-06-28 00:44:23 +0800

    add repeated event

--- scheduler/events/urls.py
+++ scheduler/events/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
-from .views import nusmod
+from .views import nusmod, get_event_group_names, event_group
 
-urlpatterns = [path("nusmod/<str:username>", nusmod)]
+urlpatterns = [path("nusmod/<str:username>", nusmod),
+               path("<str:username>/<str:name>", event_group),
+               path("<str:username>", get_event_group_names)]
