a4e72faaa7e7d86ab067e4f4cac3217fba5ef629
ZoroarkDarkrai <fidellawi@gmail.com>
2020-07-06 20:31:19 +0800

    change day to month

--- scheduler/calendars/urls.py
+++ scheduler/calendars/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import day_events, all_events
+from .views import day_events, all_events, month_events
 
 urlpatterns = [
     path('<str:username>/<int:month>/<int:day>', day_events),
+    path('<str:username>/<int:month>', month_events),
     path('<str:username>', all_events),
 ]
