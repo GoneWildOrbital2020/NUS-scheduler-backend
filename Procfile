df5cbc598ac5ae378a2760a0ecba77eab07ba664
ZoroarkDarkrai <fidellawi@gmail.com>
2020-07-15 15:23:05 +0800

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir scheduler scheduler.wsgi
--- scheduler/scheduler/settings.py
+++ scheduler/scheduler/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,3 +163,5 @@ JWT_AUTH = {
 AUTH_USER_MODEL = "users.UserCustom"
 
 SESSION_COOKIE_SECURE = False
+
+django_heroku.settings(locals())
