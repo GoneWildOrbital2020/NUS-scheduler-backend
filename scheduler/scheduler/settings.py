395f72ee668511353a447dd63208bd698d011c62
AudreyFelicio <audreyanwar@gmail.com>
2020-07-22 23:20:27 +0800

    configure to allow download files on frontend

--- scheduler/scheduler/settings.py
+++ scheduler/scheduler/settings.py
@@ -190,5 +190,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'scheduler.storage_backends.MediaStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
